d7b0172929a5b6b9b4fd567bf1190248172f72b2
ComeToPapa <ilyabarskih1@gmail.com>
2022-11-16 17:27:58 +0700

    created scrollable console text

--- Assets/ConsoleTextLogger.cs
+++ Assets/ConsoleTextLogger.cs
@@ -15,25 +15,33 @@ public class ConsoleTextLogger : MonoBehaviour
     private void Start()
     {
         gameConsole = gameObject.GetComponent<TextMeshProUGUI>();
+
+        AddLineOfText("hit from the left", ConsolePlace.Left);
+        AddLineOfText("hit from the right", ConsolePlace.Right);
+        AddLineOfText("hit from the commentator", ConsolePlace.Neutral);
     }
 
     public void AddLineOfText(string line, ConsolePlace place) 
     {
         string startTags = "";
+        string endTags = "";
         switch (place) 
         {
             case ConsolePlace.Left:
-                    startTags = "<align=left><color=#" + ColorUtility.ToHtmlStringRGBA(leftCornerColor) + ">";
+                startTags = "<align=left><color=#" + ColorUtility.ToHtmlStringRGB(leftCornerColor) + ">";
+                endTags = "</color></align>";
                 break;
             case ConsolePlace.Right:
-                    startTags = "<align=right><color=#" + ColorUtility.ToHtmlStringRGBA(rightCornerColor) + ">";
+                startTags = "<align=right><color=#" + ColorUtility.ToHtmlStringRGB(rightCornerColor) + ">";
+                endTags = "</color></align>";
                 break;
             default:
-                    startTags = "<align=center>";
+                startTags = "<align=center>";
+                endTags = "</align>";
                 break;  
         }
 
 
-        gameConsole.text += startTags + line + "\n";
+        gameConsole.text += startTags + line + endTags + "\n";
     }
 }
