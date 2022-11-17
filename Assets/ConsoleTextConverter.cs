567aa82b93aceb7d1a691cbefc94730ffed79a3c
ComeToPapa <ilyabarskih1@gmail.com>
2022-11-17 12:00:20 +0700

    init a branch

---
+++ Assets/ConsoleTextConverter.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ConsoleTextConverter
+{
+    public static string WrapTextInTags(string text, ConsolePlace place)
+    {
+
+        return "null";
+    }
+}
