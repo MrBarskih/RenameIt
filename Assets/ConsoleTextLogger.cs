f203ad28cda5d0fe8f8c2d5f92366a16672424d3
ComeToPapa <ilyabarskih1@gmail.com>
2022-11-21 16:28:44 +0700

    consoleOutputCreated

--- Assets/ConsoleTextLogger.cs
+++ Assets/ConsoleTextLogger.cs
@@ -6,42 +6,24 @@ using UnityEngine;
 
 public class ConsoleTextLogger : MonoBehaviour
 {
-    public Color leftCornerColor;
-    public Color rightCornerColor;
-
     private TextMeshProUGUI gameConsole;
+    private ConsoleTextConverter textConverter;
 
 
     private void Start()
     {
-        gameConsole = gameObject.GetComponent<TextMeshProUGUI>();
-
-        AddLineOfText("hit from the left", ConsolePlace.Left);
-        AddLineOfText("hit from the right", ConsolePlace.Right);
-        AddLineOfText("hit from the commentator", ConsolePlace.Neutral);
+        textConverter= FindObjectOfType<ConsoleTextConverter>().GetComponent<ConsoleTextConverter>();
+        gameConsole= GetComponent<TextMeshProUGUI>();
     }
 
-    public void AddLineOfText(string line, ConsolePlace place) 
+    public void AddText(string text, WrapTags tags) 
     {
-        string startTags = "";
-        string endTags = "";
-        switch (place) 
-        {
-            case ConsolePlace.Left:
-                startTags = "<align=left><color=#" + ColorUtility.ToHtmlStringRGB(leftCornerColor) + ">";
-                endTags = "</color></align>";
-                break;
-            case ConsolePlace.Right:
-                startTags = "<align=right><color=#" + ColorUtility.ToHtmlStringRGB(rightCornerColor) + ">";
-                endTags = "</color></align>";
-                break;
-            default:
-                startTags = "<align=center>";
-                endTags = "</align>";
-                break;  
-        }
+        text = textConverter.WrapTextInTags(text, tags);
+        gameConsole.text += text;
+    }
 
-
-        gameConsole.text += startTags + line + endTags + "\n";
+    public void NextLine()
+    {
+        gameConsole.text += "\n";
     }
 }
